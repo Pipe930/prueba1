3608de503ed618349ff5d72c213d7743c027b186
Pipe930 <felip.sanchezc@duocuc.cl>
2022-09-14 09:23:49 -0300

    creacion del proyecto registro asistencias

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AlumnoComponent } from './componentes/alumno/alumno.component';
+import { FormularioAlumnoComponent } from './componentes/formulario-alumno/formulario-alumno.component';
+import { FormularioSeccionComponent } from './componentes/formulario-seccion/formulario-seccion.component';
+import { ListaHistorialComponent } from './componetes/lista-historial/lista-historial.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AlumnoComponent,
+    FormularioAlumnoComponent,
+    FormularioSeccionComponent,
+    ListaHistorialComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
